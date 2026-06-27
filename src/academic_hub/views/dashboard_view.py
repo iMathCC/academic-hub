24aad65021ddbcbc8aaf015eee7f2f75b05ffa74
Dev | Matheus Chaves <matheus.costa.chaves.dev@gmail.com>
2026-06-27 16:26:37 -0300

    Implementação DashboardService e renderização dinâmica

--- src/academic_hub/views/dashboard_view.py
+++ src/academic_hub/views/dashboard_view.py
@@ -1,6 +1,14 @@
 from flask import render_template
+from academic_hub.services.dashboard_service import DashboardService
 
 class DashboardView:
+
+    def __init__(self):
+        self.service = DashboardService()
+
     def dashboard(self):
-        return render_template("dashboard.html")
+        dashboard = self.service.carregar_dashboard()
+
+        return render_template("dashboard.html",
+                               dashboard=dashboard)
 
